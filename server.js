3cb0f9926b3125102971a02f215ae5c3fb425a43
Abhishek <abhishekja.ec18@nsut.ac.in>
2021-07-25 18:29:45 +0530

    Initaial

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require('express')
+const app=express()
+const path=require('path')
+const connectDB=require('./config/db')
+connectDB()
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.set('views',path.join(__dirname,'/views'))
+app.set('view engine','ejs')
+
+
+app.use('/api/files',require('./routes/files'))
+app.use('/files',require('./routes/show'))
+app.use('/files/download',require('./routes/download'))
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.listen(process.env.PORT || 3000)
